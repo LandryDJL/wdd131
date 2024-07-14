602681412425b4e08f17189aabf640774ef45100
landryDJL <landrydallydjl@gmail.com>
2024-07-14 09:50:27 +0000

    W04-filtered-temples and siteplan

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,12 @@
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Get the date the document was last modified
+const lastModifiedDate = document.lastModified;
+
+// Output the current year in the first paragraph of the footer
+document.getElementById('current-year').innerHTML = `© ${currentYear} - `;
+
+// Output the last modified date in the second paragraph of the footer
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
